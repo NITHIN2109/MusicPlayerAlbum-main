35dd82f286a8872c2c393effd862435478ef53a8
Alampally Nithin kumar <110482153+NITHIN2109@users.noreply.github.com>
2024-03-11 13:17:44 +0000

    updated control.js

--- server/controllers/control.js
+++ server/controllers/control.js
@@ -1,15 +1,16 @@
 const db = require("../model/loginmodel");
-// const songdb = require("../model/songs");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
+const path = require("path");
 
 require("dotenv").config();
+
 exports.createuser = (req, res) => {
-  const userdeatils = req.body;
-  if (!userdeatils.Name || !userdeatils.Email || !userdeatils.Password) {
+  const userdetails = req.body;
+  if (!userdetails.Name || !userdetails.Email || !userdetails.Password) {
     return res.json({ Error: "Include all details" });
   }
-  db.createuser(userdeatils, (result, err) => {
+  db.createuser(userdetails, (result, err) => {
     if (err) {
       return res
         .status(500)
@@ -23,31 +24,31 @@ exports.createuser = (req, res) => {
 };
 
 exports.LoginUser = (req, res) => {
-  let loginDetails = req.body;
-  console.log(loginDetails);
+  const loginDetails = req.body;
   db.LoginUser(loginDetails, (err, result) => {
     if (err) {
       console.log(err);
       return res.status(500).json({ Error: "Internal Server Error" });
     } else if (result.Message === "Login Successfull") {
-      console.log(result)
-      const name = result[0].Name;
+      const user = result.user;
+      const name = user.Name;
+      const role = user.role;
       const token = jwt.sign(
-        { name: name, role: result[0].role },
+        { name: name, role: role },
         process.env.Secret_key,
         {
           expiresIn: "1d",
         }
       );
       res.cookie("token", token);
-      const isadmin = result[0].role === "admin";
+      const isadmin = role === "admin";
       return res
         .status(200)
         .json({ message: "Login Successfull", isadmin: isadmin });
     } else if (result.Message === "Wrong password") {
       return res.status(401).json({ message: "Wrong password" });
     } else {
-      return res.status(401).json({ message: "Inavlid Username" });
+      return res.status(401).json({ message: "Invalid Username" });
     }
   });
 };
@@ -57,7 +58,7 @@ exports.verifyUSer = (req, res) => {
   if (!token) {
     return res.status(403).json({
       auth: false,
-      message: "Notoken provided.",
+      message: "No token provided.",
     });
   }
   try {
@@ -66,7 +67,7 @@ exports.verifyUSer = (req, res) => {
     return res.status(200).json({
       auth: true,
       name: username,
-      message: "token Verfied successfull",
+      message: "Token verified successfully",
       isadmin: decode.role === "admin" ? true : false,
     });
   } catch (error) {
@@ -88,6 +89,7 @@ exports.sendImage = (req, res) => {
     res.status(404).json({ Error: "File not found" });
   });
 };
+
 exports.getalbums = (req, res) => {
   db.getalbums((err, output) => {
     if (err) {
@@ -111,8 +113,6 @@ exports.playmusic = (req, res) => {
   });
 };
 
-const path = require("path");
-
 exports.songdownload = (req, res) => {
   const songname = req.params.songid;
   const filePath = path.join(__dirname, "../songs", songname);
@@ -125,40 +125,3 @@ exports.songdownload = (req, res) => {
     console.log(result);
   });
 };
-
-// exports.likedSongs = (req, res) => {
-//   const userid = req.params.userId;
-//   songdb.likedSongs(userid, (err, songs) => {
-//     if (err) {
-//       res.status(500).json("Server Error");
-//       console.log(err);
-//     } else {
-//       res.status(200).json(songs);
-//     }
-//   });
-// };
-// exports.addLikedSong = (req, res) => {
-//   const { user_id, song_id } = req.body;
-
-//   songdb.addlikedsongs(user_id, song_id, (err, result) => {
-//     if (err) {
-//       console.error("Error adding liked song: ", err);
-//       res.status(500).json({ error: "Internal server error" });
-//       return;
-//     }
-//     res.status(201).json({ message: "Liked song added successfully" });
-//   });
-// };
-
-// exports.deleteLikedSong = (req, res) => {
-//   const { user_id, song_id } = req.body;
-
-//   songdb.deleteLikedSong(user_id, song_id, (err, result) => {
-//     if (err) {
-//       console.error("Error deleting liked song: ", err);
-//       res.status(500).json({ error: "Internal server error" });
-//       return;
-//     }
-//     res.status(200).json({ message: "Liked song deleted successfully" });
-//   });
-// };
