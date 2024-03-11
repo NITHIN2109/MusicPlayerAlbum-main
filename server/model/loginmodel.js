1e7a1bf3b871dcd0d5c5e74d8e4a9bdbd80dbff0
Alampally Nithin kumar <110482153+NITHIN2109@users.noreply.github.com>
2024-03-11 18:25:36 +0530

    Update loginmodel.js

--- server/model/loginmodel.js
+++ server/model/loginmodel.js
@@ -1,65 +1,156 @@
+// const db = require("./model");
+// exports.createuser = (userdeatils, callback) => {
+//   let Name = userdeatils.Name;
+//   let Email = userdeatils.Email;
+//   let Password = userdeatils.Password;
+//   let query1 = `Select * from users where Email='${Email}'`;
+//   db.query(query1, (err, result) => {
+//     if (err) {
+//       callback(null, err);
+//       return;
+//     }
+//     console.log(result);
+
+//     if (result.length > 0 || result) {
+//       // console.log(result.length);
+//       callback({ alreadyExists: true }, null);
+//       console.log(result);
+//       return;
+//     }
+//     let query = `Insert into users(Name,Email,Password) values ("${Name}","${Email}","${Password}")`;
+
+//     db.query(query, (err) => {
+//       if (err) {
+//         // console.log(err);
+//         callback(null, err);
+//         return;
+//       }
+//       callback();
+//       return;
+//     });
+//   });
+// };
+// exports.LoginUser = (Logindata, callback) => {
+//   const { Email, Password } = Logindata;
+
+//   let query = `SELECT * FROM users WHERE Email='${Email}'`;
+//   db.query(query, (err, result) => {
+//      console.log(result);
+//     if (err) {
+//       callback(err, null);
+//     }
+    
+//     if (result.length > 0 || result) {
+//       console.log(result);
+//       if (result[0].Password === Password) {
+//         callback(null, { Message: "Login Successfull", ...result });
+//         return;
+//       } else {
+//          console.log(result);
+//         callback(null, { Message: "Wrong password" });
+//         return;
+//       }
+//     } else {
+//       console.log(result);
+//       callback(null, { Message: "Invalid UserName" });
+//       return;
+//     }
+//   });
+// };
+
+// exports.getalbums = (callback) => {
+//   let query = `
+//     SELECT a.*, 
+//            GROUP_CONCAT(s.song_name) AS song_filenames,
+//            GROUP_CONCAT(s.song_id) AS song_ids
+//     FROM albums a
+//     LEFT JOIN songs s ON a.id = s.album_id
+//     GROUP BY a.id
+//   `;
+//   db.query(query, (err, result) => {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       const albumsWithSongs = result.map((album) => {
+//         const song_filenames = album.song_filenames
+//           ? album.song_filenames.split(",")
+//           : [];
+//         const song_ids = album.song_ids ? album.song_ids.split(",") : [];
+//         const songs = song_filenames.map((song_filename, index) => ({
+//           song_filename,
+//           song_id: song_ids[index],
+//         }));
+//         return {
+//           id: album.id,
+//           title: album.title,
+//           genre: album.genre,
+//           artist: album.artist,
+//           coverImage: album.coverImage,
+//           releaseYear: album.releaseYear,
+//           songs,
+//         };
+//       });
+//       callback(null, albumsWithSongs);
+//     }
+//   });
+// };
 const db = require("./model");
-exports.createuser = (userdeatils, callback) => {
-  let Name = userdeatils.Name;
-  let Email = userdeatils.Email;
-  let Password = userdeatils.Password;
-  let query1 = `Select * from users where Email='${Email}'`;
+
+exports.createuser = (userdetails, callback) => {
+  const { Name, Email, Password } = userdetails;
+  const query1 = `SELECT * FROM users WHERE Email='${Email}'`;
+
   db.query(query1, (err, result) => {
     if (err) {
-      callback(null, err);
-      return;
+      return callback(null, err);
     }
-    console.log(result);
 
-    if (result.length > 0 || result) {
-      // console.log(result.length);
-      callback({ alreadyExists: true }, null);
-      console.log(result);
-      return;
+    if (result.length > 0) {
+      return callback({ alreadyExists: true }, null);
     }
-    let query = `Insert into users(Name,Email,Password) values ("${Name}","${Email}","${Password}")`;
+
+    const query = `INSERT INTO users (Name, Email, Password) VALUES ("${Name}", "${Email}", "${Password}")`;
 
     db.query(query, (err) => {
       if (err) {
-        // console.log(err);
-        callback(null, err);
-        return;
+        return callback(null, err);
       }
       callback();
-      return;
     });
   });
 };
+
 exports.LoginUser = (Logindata, callback) => {
   const { Email, Password } = Logindata;
+  const query = `SELECT * FROM users WHERE Email='${Email}'`;
 
-  let query = `SELECT * FROM users WHERE Email='${Email}'`;
   db.query(query, (err, result) => {
-     console.log(result);
     if (err) {
-      callback(err, null);
+      return callback(err, null);
     }
-    
-    if (result.length > 0 || result) {
-      console.log(result);
-      if (result[0].Password === Password) {
-        callback(null, { Message: "Login Successfull", ...result });
-        return;
-      } else {
-         console.log(result);
-        callback(null, { Message: "Wrong password" });
-        return;
-      }
+
+    if (!result || result.length === 0) {
+      return callback(null, { Message: "Invalid UserName" });
+    }
+
+    const user = result[0];
+
+    if (user.Password === Password) {
+      const userInfo = {
+        id: user.id,
+        Name: user.Name,
+        Email: user.Email,
+        // Add other necessary user information here
+      };
+      return callback(null, { Message: "Login Successfull", user: userInfo });
     } else {
-      console.log(result);
-      callback(null, { Message: "Invalid UserName" });
-      return;
+      return callback(null, { Message: "Wrong password" });
     }
   });
 };
 
 exports.getalbums = (callback) => {
-  let query = `
+  const query = `
     SELECT a.*, 
            GROUP_CONCAT(s.song_name) AS song_filenames,
            GROUP_CONCAT(s.song_id) AS song_ids
@@ -69,28 +160,26 @@ exports.getalbums = (callback) => {
   `;
   db.query(query, (err, result) => {
     if (err) {
-      callback(err, null);
-    } else {
-      const albumsWithSongs = result.map((album) => {
-        const song_filenames = album.song_filenames
-          ? album.song_filenames.split(",")
-          : [];
-        const song_ids = album.song_ids ? album.song_ids.split(",") : [];
-        const songs = song_filenames.map((song_filename, index) => ({
-          song_filename,
-          song_id: song_ids[index],
-        }));
-        return {
-          id: album.id,
-          title: album.title,
-          genre: album.genre,
-          artist: album.artist,
-          coverImage: album.coverImage,
-          releaseYear: album.releaseYear,
-          songs,
-        };
-      });
-      callback(null, albumsWithSongs);
+      return callback(err, null);
     }
+    const albumsWithSongs = result.map((album) => {
+      const song_filenames = album.song_filenames ? album.song_filenames.split(",") : [];
+      const song_ids = album.song_ids ? album.song_ids.split(",") : [];
+      const songs = song_filenames.map((song_filename, index) => ({
+        song_filename,
+        song_id: song_ids[index],
+      }));
+      return {
+        id: album.id,
+        title: album.title,
+        genre: album.genre,
+        artist: album.artist,
+        coverImage: album.coverImage,
+        releaseYear: album.releaseYear,
+        songs,
+      };
+    });
+    callback(null, albumsWithSongs);
   });
 };
+
